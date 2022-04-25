8fcded745f9108973fcc27d21af613910a883f76
akartell <tothkartal7@gmail.com>
2022-04-25 13:13:07 +0200

    első és második feladat

--- szamla.py
+++ szamla.py
@@ -22,49 +22,38 @@ def beolvasas():
     del(adatok[-1])
     #inté teszi őket
     for i in range(len(adatok)):
-        for y in range(len(adatok[i])):
+        for y in range(1,len(adatok[i])):
             adatok[i][y]=int(adatok[i][y])
-    print(adatok)
+
     #telefonszám, kezdő óra, kezdő perc, kezdő másodperc
 beolvasas()
 #1.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+szam=input("Mondj egy telefonszámot: ")
+letezik=False
+if szam[0:2]=="39" or szam[0:2]=="41" or szam[0:2]=="71":
+    for i in range(len(adatok)):
+        if adatok[i][0]==szam:
+            letezik=True
+            break
+    if letezik:
+        print("Létező mobilszám!")
+    else:
+        print("Nem létező mobilszám!")
+else:
+    print("Nem mobilszám!")
 
 
 
 #2.
+hivas=[]
+hivas.append(int(input("Hívás eleje: óra: ")))
+hivas.append(int(input("Hívás eleje: perc: ")))
+hivas.append(int(input("Hívás eleje: másodperc: ")))
+hivas.append(int(input("Hívás vége: óra: ")))
+hivas.append(int(input("Hívás vége: perc: ")))
+hivas.append(int(input("Hívás vége: másodperc: ")))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(f"{((hivas[3]-hivas[0])*60)+(hivas[4]-hivas[1])} perc hosszúság volt a hívás.")
 
 #3.
 
